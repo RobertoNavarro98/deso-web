8911a0a1966a1690eed3a5a020ffe2702fdd8899
Roberto Navarro <robertonavarro98@gmail.com>
2025-11-10 21:43:09 -0600

    Agregar versión v2 (industrial pro) en src_v2

--- src_v2/components/Footer.jsx
+++ src_v2/components/Footer.jsx
@@ -8,7 +8,7 @@ export default function Footer(){
         <div className="flex gap-4">
           <a href="#" className="text-gray-400 hover:text-white">LinkedIn</a>
           <a href="#" className="text-gray-400 hover:text-white">Facebook</a>
-          <a href="mailto:contacto@deso.com.mx" className="text-gray-400 hover:text-white">contacto@deso.com.mx</a>
+          <a href="mailto:contacto@deso.com.mx" className="text-gray-400 hover:text-white">navarro.roberto1998@outlook.com</a>
         </div>
       </div>
     </footer>
